18901a2cba5862a002f30d23b3917c73b9870976
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-12-01 22:07:30 +0100

    added anonymouserequired

---
+++ src/components/roles/AnonoymousRequired.tsx
@@ -0,0 +1,23 @@
+import {
+    Navigate, Outlet, useSearchParams
+} from 'react-router-dom';
+import { useAppSelector } from 'redux/hooks';
+
+
+export default function AnonymousRequired() {
+
+    const [searchParams] = useSearchParams();
+
+    const backUrl = searchParams.get("backUrl");
+
+    const { user } = useAppSelector((state) => state.account);
+
+
+    if (user == null) {
+        return <Outlet />;
+    } else {
+
+        return <Navigate replace={true} to={backUrl ?? "/account"} />
+    }
+
+}
